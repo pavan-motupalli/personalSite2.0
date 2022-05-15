82cbe3e98c570646970e975797910935906db073
Pavan Motupalli <pmotupalli14@gmail.com>
2022-05-15 14:32:19 -0500

    Particles oldie but goldie

--- src/App.js
+++ src/App.js
@@ -1,24 +1,11 @@
-import logo from './logo.svg';
+import Particles  from 'react-particles-js';
+import config from './config';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    
+    <Particles className="particles" params={config.particles} />
   );
 }
 
